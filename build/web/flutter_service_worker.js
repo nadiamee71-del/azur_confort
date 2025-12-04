eb3bed16c7a94c1bea7e7759bb838b7152077615
nadiamee71-del <nadiamee71@gmail.com>
2025-12-04 12:35:37 +0100

    🔄 Rebuild complet - Service Électricité

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f9cfe3aa6ac50ee12d3daaa981270063
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9b2b940ee92bde5e655ddbea2980ffcb",
+"flutter_bootstrap.js": "2ce9b346c89eca41d1c0ec9223e317ce",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
