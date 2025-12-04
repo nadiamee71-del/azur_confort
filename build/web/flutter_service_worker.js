740dbf7e577c81774288f6f265891415477489fa
nadiamee71-del <nadiamee71@gmail.com>
2025-12-04 11:49:37 +0100

    🔄 Rebuild complet avec sélecteur de langue

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "141a5290a07e48a8332a29056413a87a
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "905919c30c75f5fe52da3f6a3cb9dfa7",
+"flutter_bootstrap.js": "337823e4390b0c8e34e48e46ec44bf01",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
